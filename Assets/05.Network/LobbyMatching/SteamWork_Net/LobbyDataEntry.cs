2dc355a69bf46257fc587ef3a48f2364923258a6
pqowp90 <77601017+pqowp90@users.noreply.github.com>
2023-02-21 22:17:02 +0900

    ui 빌더
    d

--- Assets/05.Network/LobbyMatching/SteamWork_Net/LobbyDataEntry.cs
+++ Assets/05.Network/LobbyMatching/SteamWork_Net/LobbyDataEntry.cs
@@ -1,26 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using Steamworks;
-using UnityEngine.UIElements;
 
 public class LobbyDataEntry : MonoBehaviour
 {
     public CSteamID lobbyID;
     public string lobbyName;
-    public void SetLobbyData(){
-        
-        if(lobbyName == ""){
-            lobbyName = "Empty";
-        }
-        SetLobbyLable();
-    }
+
     public void JoinLobby(){
         SteamLobby.Instance.JoinLobby(lobbyID);
     }
-    public void SetLobbyLable()
-    {
-        var button = new Button(JoinLobby);
-        LobbyButton.lobbyScrollView.Add(button);
-    }
+
 }
