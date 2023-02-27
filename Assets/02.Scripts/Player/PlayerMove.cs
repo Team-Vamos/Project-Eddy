6d650b4c79bcc068d452bdf1ec8da36c61d5b604
pqowp90 <77601017+pqowp90@users.noreply.github.com>
2023-02-27 20:16:29 +0900

    1
    1

---
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EventManagers;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Vector3 inputDir;
+    private Vector3 realMove = Vector3.zero;
+    [SerializeField]
+    private bool canMove = false;
+    private PlayerObjectControler playerObjectControler;
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        playerObjectControler = GetComponent<PlayerObjectControler>();
+        Debug.Log("Start");
+        EventManager.StartListening(NetManager.StartNetworkCallback, SetNetwork);
+    }
+    private void SetNetwork()
+    {
+        Debug.Log("SetNetwork");
+        if (playerObjectControler.isLocal)
+        {
+            canMove = true;
+        }
+    }
+    private void Update()
+    {
+        if (canMove) Move();
+    }
+    private void FixedUpdate() {
+        if (canMove)
+            _rb.velocity = realMove;
+    }
+    private void Move()
+    {
+        inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        realMove = /* [stat *] */inputDir.normalized;// * Time.deltaTime;
+    }
+}
