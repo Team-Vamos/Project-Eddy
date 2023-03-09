bec3140005d82ff499da132fc6a5b3053e2d1bd8
Bwasabe <jixg20@gmail.com>
2023-03-10 00:34:26 +0900

    ShopUSS

---
+++ Assets/07.UIToolkit/NexusHP/NexusUIDocument.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(UIDocument))]
+public class NexusUIDocument : MonoBehaviour
+{
+    // private UIDocument _document;
+    // private void Awake() {
+    //     _document = GetComponent<UIDocument>();
+    // }
+
+    // private void OnEnable() {
+    //     _document
+    // }
+}
