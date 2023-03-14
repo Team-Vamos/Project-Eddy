a40b2e2cb27330ad6be95fe65d50e52afb501d22
KBluePurple <42506968+BlWon@users.noreply.github.com>
2023-03-14 14:52:55 +0900

    풀 매니저 엄청난 최적화

--- Assets/02.Scripts/Util/PoolManager/PoolStorage.cs
+++ Assets/02.Scripts/Util/PoolManager/PoolStorage.cs
@@ -6,6 +6,7 @@ public class PoolStorage : MonoBehaviour
 {
     private static readonly Dictionary<GameObject, Stack<GameObject>> PooledObjects = new();
     private static readonly Dictionary<GameObject, List<GameObject>> RegisteredObjects = new();
+    private static readonly Dictionary<GameObject, PoolObject> PoolObjects = new();
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Init()
@@ -13,7 +14,7 @@ public class PoolStorage : MonoBehaviour
         var go = new GameObject("PoolManager");
         go.AddComponent<PoolStorage>();
         go.hideFlags = HideFlags.HideInHierarchy;
-        
+
         SceneManager.sceneUnloaded += OnSceneUnloaded;
 
         DontDestroyOnLoad(go);
@@ -24,58 +25,41 @@ public class PoolStorage : MonoBehaviour
         var keys = new GameObject[PooledObjects.Keys.Count];
         PooledObjects.Keys.CopyTo(keys, 0);
         foreach (var key in keys)
-        {
             if (key == null)
-            {
                 PooledObjects.Remove(key);
-            }
-        }
-        
+
         var registeredKeys = new GameObject[RegisteredObjects.Keys.Count];
         RegisteredObjects.Keys.CopyTo(registeredKeys, 0);
         foreach (var key in registeredKeys)
-        {
             if (key == null)
-            {
                 RegisteredObjects.Remove(key);
-            }
-        }
     }
 
     public static GameObject InstantiateObject(GameObject prefab, Transform parent = null)
     {
-        var go = Instantiate(prefab, parent);
-        RegisterObject(go, prefab);
-        if (go.TryGetComponent(out PoolObject _))
-        {
-            return go;
-        }
-        go.AddComponent<PoolObject>().prefab = prefab;
-        return go;
+        var instance = Instantiate(prefab, parent);
+        RegisterObject(instance, prefab);
+        if (instance.TryGetComponent(out PoolObject _)) return instance;
+        var poolObject = instance.AddComponent<PoolObject>();
+        poolObject.prefab = prefab;
+        poolObject.Init();
+        PoolObjects.TryAdd(instance, poolObject);
+        return instance;
     }
 
     private static void RegisterObject(GameObject go, GameObject prefab, bool isPooled = false)
     {
-        if (!RegisteredObjects.ContainsKey(prefab))
-        {
-            RegisteredObjects.Add(prefab, new List<GameObject>());
-        }
+        if (!RegisteredObjects.ContainsKey(prefab)) RegisteredObjects.Add(prefab, new List<GameObject>());
 
         go.name = $"{prefab.name} [{RegisteredObjects[prefab].Count}]";
         RegisteredObjects[prefab].Add(go);
 
-        if (isPooled)
-        {
-            go.SetActive(false);
-        }
+        if (isPooled) go.SetActive(false);
     }
 
     public static GameObject GetObject(GameObject prefab, Transform parent = null)
     {
-        if (!PooledObjects.ContainsKey(prefab))
-        {
-            PooledObjects.Add(prefab, new Stack<GameObject>());
-        }
+        if (!PooledObjects.ContainsKey(prefab)) PooledObjects.Add(prefab, new Stack<GameObject>());
 
         if (PooledObjects[prefab].Count > 0)
         {
@@ -86,7 +70,7 @@ public class PoolStorage : MonoBehaviour
         }
 
         var instance = InstantiateObject(prefab, parent);
-        var poolObject = instance.GetComponent<PoolObject>();
+        var poolObject = PoolObjects[instance];
         poolObject.onInit.Invoke();
         instance.hideFlags = HideFlags.None;
         return instance;
@@ -94,19 +78,19 @@ public class PoolStorage : MonoBehaviour
 
     public static void ReturnObject(GameObject instance)
     {
-        var poolObject = instance.GetComponent<PoolObject>();
-        poolObject.onReturn.Invoke();
-        var prefab = poolObject.prefab;
-        if (prefab == null)
+        if (PoolObjects.TryGetValue(instance, out var poolObject))
         {
-            Debug.LogError("Prefab not found for instance " + instance.name);
+            poolObject.onReturn.Invoke();
+        }
+        else
+        {
+            Debug.LogWarning("Prefab not found for instance " + instance.name);
+            Destroy(instance);
             return;
         }
 
-        if (!PooledObjects.ContainsKey(prefab))
-        {
-            PooledObjects.Add(prefab, new Stack<GameObject>());
-        }
+        var prefab = poolObject.prefab;
+        if (!PooledObjects.ContainsKey(prefab)) PooledObjects.Add(prefab, new Stack<GameObject>());
 
         PooledObjects[prefab].Push(instance);
         instance.SetActive(false);
